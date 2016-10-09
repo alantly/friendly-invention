fff5dba6430598ceec0c8cef4852a1f59e0f9a21
Alan <alanly211@gmail.com>
2016-10-09 00:18:25 -0700

    fixed min property to be always true on swiftdown

--- datastructures/heap.py
+++ datastructures/heap.py
@@ -7,6 +7,16 @@ class Heap(object):
         self.heapList = [0]
         self.currentSize = 0
 
+    def checkMinProperty(self):
+        for i in range(1, self.currentSize+1):
+            leftChild = self.getChildPositionLeft(i)
+            rightChild = self.getChildPositionRight(i)
+            if leftChild <= self.currentSize and self.heapList[i] > self.heapList[leftChild]:
+                return False
+            elif rightChild <= self.currentSize and self.heapList[i] > self.heapList[rightChild]:
+                return False
+        return True
+
     def getParentPosition(self, currentPosition):
         return currentPosition/2;
 
@@ -65,14 +75,23 @@ class Heap(object):
 
     def swiftDown(self, currentPosition):
         top = self.heapList[currentPosition]
+        minChild = self.getMinChild(currentPosition)
+        if minChild != -1 and top > self.heapList[minChild]:
+            self.swap(currentPosition, minChild)
+            self.swiftDown(minChild)
+
+    def getMinChild(self, currentPosition):
         leftChild = self.getChildPositionLeft(currentPosition)
         rightChild = self.getChildPositionRight(currentPosition)
-        if leftChild <= self.currentSize and top > self.heapList[leftChild]:
-            self.swap(currentPosition, leftChild)
-            self.swiftDown(leftChild)
-        elif rightChild <= self.currentSize and top > self.heapList[rightChild]:
-            self.swap(currentPosition, rightChild)
-            self.swiftDown(rightChild)
+        if rightChild <= self.currentSize:
+            if self.heapList[leftChild] < self.heapList[rightChild]:
+                return leftChild
+            else:
+                return rightChild
+        elif leftChild <= self.currentSize:
+            return leftChild
+        else:
+            return -1
 
 ##############################
 import unittest
@@ -82,6 +101,7 @@ class TestLinkLists(unittest.TestCase):
     def setUp(self):
         self.heap = Heap()
         self.heap.insert(6).insert(9).insert(2).insert(4)
+        self.assertTrue(self.heap.checkMinProperty())
 
     def test_printData(self):
         self.assertEqual("2469", self.heap.printData())
@@ -103,16 +123,19 @@ class TestLinkLists(unittest.TestCase):
         self.assertEqual("142956", self.heap.printData())
         self.heap.insert(10)
         self.assertEqual("14295610", self.heap.printData())
+        self.assertTrue(self.heap.checkMinProperty())
 
     def test_insert_2(self):
         self.assertEqual("2469", self.heap.printData())
         self.heap.insert(None)
         self.assertEqual("2469", self.heap.printData())
+        self.assertTrue(self.heap.checkMinProperty())
 
     def test_removeMin_1(self):
         self.assertEqual("2469", self.heap.printData())
         self.heap.removeMin()
         self.assertEqual("496", self.heap.printData())
+        self.assertTrue(self.heap.checkMinProperty())
 
     def test_removeMin_2(self):
         self.assertEqual("2469", self.heap.printData())
@@ -122,6 +145,15 @@ class TestLinkLists(unittest.TestCase):
         self.assertEqual(9, self.heap.removeMin())
         self.assertEqual(None, self.heap.removeMin())
         self.assertEqual("", self.heap.printData())
+        self.assertTrue(self.heap.checkMinProperty())
+
+    def test_removeMin_3(self):
+        heap = Heap()
+        heap.insert(1).insert(3).insert(2).insert(7)
+        self.assertEqual("1327", heap.printData())
+        self.assertEqual(1, heap.removeMin())
+        self.assertEqual("237", heap.printData())
+        self.assertTrue(heap.checkMinProperty())
 
     def test_insert_removeMin(self):
         heap = Heap()
@@ -129,6 +161,7 @@ class TestLinkLists(unittest.TestCase):
         self.assertEqual(5, heap.removeMin())
         heap.insert(6).insert(2).insert(1)
         self.assertEqual("162", heap.printData())
+        self.assertTrue(heap.checkMinProperty())
 
 if __name__ == '__main__':
     unittest.main()
