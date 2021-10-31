7cc0cd545403c91938f71294f6dd2ab32926f4b1
Alan <alan.calcifer@gmail.com>
2021-10-31 00:22:49 -0700

    update trie

--- trees/trie.rb
+++ trees/trie.rb
@@ -2,7 +2,7 @@ class TrieNode
   attr_accessor :characters, :end_of_word
   def initialize()
     @characters = {}
-    @end_of_word = false
+    @end_of_word = 0
   end
 end
 
@@ -25,7 +25,7 @@ class Trie
     if remaining_chars.length > 0
       _insert(node.characters[first_char], remaining_chars)
     else
-      node.characters[first_char].end_of_word = true
+      node.characters[first_char].end_of_word += 1
     end
   end
 
@@ -42,7 +42,7 @@ class Trie
     if node.characters[first_char]
       if remaining_chars.length == 0
         return true if prefix_only
-        return node.characters[first_char].end_of_word
+        return node.characters[first_char].end_of_word > 0
       else
         return _search(node: node.characters[first_char], word: remaining_chars, prefix_only: prefix_only)
       end
@@ -53,6 +53,70 @@ class Trie
 
   def starts_with(prefix)
     _search(node: @head, word: prefix, prefix_only: true)
+  end
+
+  def erase(word)
+    _erase(node: @head, word: word)
+  end
+
+  def _erase(node:, word:)
+    return false if word.length == 0
+
+    first_char = word[0]
+    remaining_chars = word[1..]
+
+    if node.characters[first_char]
+      char_node = node.characters[first_char]
+      if remaining_chars.length == 0 && char_node.end_of_word > 0
+        char_node.end_of_word -= 1
+        delete = true
+      else
+        delete = _erase(node: node.characters[first_char], word: remaining_chars)
+      end
+      if delete && char_node.characters.size == 0 && char_node.end_of_word == 0
+        node.characters.delete(first_char)
+      end
+      return delete
+    else
+      return false
+    end
+  end
+
+  def count_words_equal_to(word)
+    _count_words_equal_to(node: @head, word: word)
+  end
+
+  def _count_words_equal_to(node:, word:, count_remaining: false)
+    return 0 if word.length == 0
+
+    first_char = word[0]
+    remaining_chars = word[1..]
+
+    if node.characters[first_char]
+      if remaining_chars.length == 0
+        if count_remaining
+          return sum_all_words(node: node.characters[first_char])
+        else
+          return node.characters[first_char].end_of_word
+        end
+      else
+        return _count_words_equal_to(node: node.characters[first_char], word: remaining_chars, count_remaining: count_remaining)
+      end
+    else
+      return 0
+    end
+  end
+
+  def sum_all_words(node:)
+    result = node.end_of_word
+    node.characters.each do |char, char_node|
+      result += sum_all_words(node: char_node)
+    end
+    result
+  end
+
+  def count_words_starting_with(prefix)
+    _count_words_equal_to(node: @head, word: prefix, count_remaining: true)
   end
 end
 
@@ -110,6 +174,94 @@ describe Trie do
       it "returns false when prefix does not match" do
         @trie.insert("apple")
         _(@trie.starts_with("apa")).must_equal false
+      end
+    end
+  end
+
+  describe "erase" do
+    describe "success" do
+      it "returns true on erase" do
+        @trie.insert("apple")
+        _(@trie.search("apple")).must_equal true
+
+        _(@trie.erase("apple")).must_equal true
+        _(@trie.search("apple")).must_equal false
+        _(@trie.starts_with("app")).must_equal false
+        _(@trie.starts_with("a")).must_equal false
+      end
+    end
+
+    describe "failures" do
+      it "returns false when attempting to delete something that does not exist" do
+        _(@trie.erase("app")).must_equal false
+      end
+
+      it "returns false when attempting to delete a prefix" do
+        @trie.insert("apple")
+        _(@trie.erase("app")).must_equal false
+      end
+
+      it "returns false when attempting to delete a superset word" do
+        @trie.insert("apple")
+        _(@trie.erase("apples")).must_equal false
+      end
+    end
+  end
+
+  describe "count_words_equal_to" do
+    describe "success" do
+      it "returns correct number of words" do
+        @trie.insert("apple")
+        @trie.insert("apple")
+        @trie.insert("apple")
+
+        _(@trie.count_words_equal_to("apple")).must_equal 3
+      end
+
+      it "returns correct number of words after erase" do
+        @trie.insert("apple")
+        @trie.insert("apple")
+        @trie.insert("apple")
+        @trie.erase("apple")
+
+        _(@trie.count_words_equal_to("apple")).must_equal 2
+      end
+    end
+
+    describe "failures" do
+      it "returns 0 when words do not exist" do
+        _(@trie.count_words_equal_to("app")).must_equal 0
+      end
+
+      it "returns 0 on prefixes" do
+        @trie.insert("apple")
+        _(@trie.count_words_equal_to("app")).must_equal 0
+      end
+    end
+  end
+
+  describe "count_words_starting_with" do
+    describe "success" do
+      it "returns correct number of words" do
+        @trie.insert("apple")
+        @trie.insert("apple")
+
+        _(@trie.count_words_starting_with("app")).must_equal 2
+      end
+
+      it "returns correct number of words after erase" do
+        @trie.insert("apple")
+        @trie.insert("apple")
+        @trie.insert("apple")
+        @trie.erase("apple")
+
+        _(@trie.count_words_starting_with("app")).must_equal 2
+      end
+    end
+
+    describe "failures" do
+      it "returns 0 when words do not exist" do
+        _(@trie.count_words_starting_with("app")).must_equal 0
       end
     end
   end
